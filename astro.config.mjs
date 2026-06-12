2d076c8507b71703d1064a158de7e5082fdfd561
mohammadceceli <46718617+mohammadceceli@users.noreply.github.com>
2026-06-12 17:34:43 +0330

    21

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tech2049.github.io', // نام کاربری خود را اینجا اصلاح کنید
-  base: '/Tech2049/',                // نام دقیق مخزن (Repository) شما
+  site: 'https://tech2049.github.io', // آدرس سایت شما
+  // گزینه base را کاملاً حذف کنید یا آن را به صورت زیر قرار دهید:
+  base: '/', 
 });
